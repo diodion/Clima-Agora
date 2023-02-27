583959915532e49aa834e4f82a24e18852824821
Diego <diegomjua@gmail.com>
2023-02-27 18:27:01 -0300

    Estrutura inicial

---
+++ js.js
@@ -0,0 +1,5 @@
+const container = document.querySelector('.container');
+const pesquisa = document.querySelector('.pesquisa-campo button');
+const climaBox = document.querySelector('.clima-container');
+const climaDetalhes = document.querySelector('.clima-detalhes');
+const error404 = document.querySelector('.nao-localizado');
